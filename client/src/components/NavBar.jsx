1040dd6430b6a5b2a45b687bd000d84798583298
Miri.Hid <mirihdz11@gmail.com>
2022-10-25 00:25:14 -0400

    create react frontend

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="container">
+      <nav className="navBar">
+        <ul className="nav-item">
+          <img src="" alt="" />
+        </ul>
+        <ul className="nav-item"></ul>
+        <ul className="nav-item"></ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
