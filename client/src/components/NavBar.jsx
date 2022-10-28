53128e3a361016551547a42184cb696de0a35a38
Miri.Hid <mirihdz11@gmail.com>
2022-10-28 00:28:52 -0400

    create nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,12 +3,27 @@ import React from "react";
 function NavBar() {
   return (
     <div className="container">
-      <nav className="navBar">
-        <ul className="nav-item">
-          <img src="" alt="" />
+      <nav className="nav">
+        <a href="#" className="logo">
+          <img src="images/logo.png" alt="" />
+        </a>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              Services
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              Classes & Activities
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              About Us
+            </a>
+          </li>
         </ul>
-        <ul className="nav-item"></ul>
-        <ul className="nav-item"></ul>
       </nav>
     </div>
   );
