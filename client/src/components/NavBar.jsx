03e2d8178e121b74e176b75ea25d9e94ca7c7fb2
Miri.Hid <mirihdz11@gmail.com>
2022-11-02 00:22:19 -0400

    add features to nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 function NavBar() {
   return (
-    <div className="container">
-      <nav className="nav">
-        <a href="#" className="logo">
-          <img src="images/logo.png" alt="" />
-        </a>
+    <div className="navBar">
+      <nav className="nav container">
         <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className="nav__link logo">
+              <img src="images/logo.png" alt="" />
+            </a>
+          </li>
           <li className="nav__item">
             <a href="#" className="nav__link">
               Services
