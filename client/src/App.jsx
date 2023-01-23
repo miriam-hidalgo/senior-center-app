95b90bccc392d8ea2f27679cbf04140e293a82a8
miriam-hidalgo <mirihdz11@gmail.com>
2023-01-23 00:33:58 -0500

    add react-router & Apps.js file change file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./App.css";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <React.StrictMode>
+      <NavBar />
+      <Hero />
+    </React.StrictMode>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import NavBar from "./components/NavBar";
 import reportWebVitals from "./reportWebVitals";
-import Hero from "./components/Hero";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <NavBar />
-    <Hero />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
